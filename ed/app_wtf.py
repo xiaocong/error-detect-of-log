fbc3a3e3412dcc7a15c9f497e78928612d1ce683
zhiqiang.guo <zhiqiang.guo@xinqitec.com>
2015-01-27 15:32:56 +0800

    modify trace template; bug fix

--- ed/app_wtf.py
+++ ed/app_wtf.py
@@ -3,12 +3,19 @@
 from utils import detect_string, gen_hashcode
 
 
+def detect_detail(content):
+    detail = detect_string(content, r'^\t(at\s+android\.util\.Log\.wtf.*)$')
+    if not detail:
+        detail = detect_string(content, r'^\t(at\s+.+)$')
+    return detail
+
+
 def app_wtf(logcat, headers):
     content = logcat.split('\n\n')
     if len(content) >= 2:
         process = detect_string(content[0], r'^Process:\s+(\w+(?:[.$_]\w+)+)')
         exception = detect_string(content[1], r'^(\w+(?:[.$_]\w+)+)')
-        detail = detect_string(content[1], r'\t(at\s+android\.util\.Log\.wtf.*)')
+        detail = detect_detail(content[1])
         if process and exception and detail:
             md5 = gen_hashcode({'issue_owner': process, 'exception': exception, 'detail': detail})
             return md5, {'issue_owner': process, 'exception': exception, 'detail': detail}, None
